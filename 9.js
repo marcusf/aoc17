9e3df799f99fca41ec0556f919836bea7b704d1f
marcusf <marcusf@spotify.com>
2017-12-26 15:33:36 +0100

    9.1 solved.

---
+++ 9.js
@@ -0,0 +1,80 @@
+const fs = require('fs');
+const INPUT = fs.readFileSync("9.txt", "utf8");
+
+const makeNode = (value, parent) => ({value: value, parent: parent, children: [] });
+
+const solve = (input) => {
+  let cleaned = clean(input);
+  let tree    = parse(cleaned);
+  let sum     = sumTree(tree, 0);
+  console.log(sum);
+}
+
+const pretty = (tree) => {
+  return '{' + tree.children.map(pretty).join(',') + '}';
+}
+
+const sumTree = (tree) => {
+  return tree.value + tree.children.reduce((acc,node) => acc+sumTree(node), 0);
+}
+
+const validate = (curr, next) => {
+  if (curr == '{') { return next != ',';
+  } else if (curr == '}') { return next == ',' || next == '{' || next == '}'
+  } else if (curr == '_') { return next == ',' || next == '}'
+  } else if (curr == ',') { return next == '_' || next == '{'
+  } else if (curr == '')  { return true
+  } else { return false;
+  }
+}
+
+const parse = (input) => {
+  let idx = 1, tree = makeNode(1, null), prev = '', chr = '', nodeStack = [], c = 1;
+  nodeStack.push(tree);
+  do {
+    let parent = nodeStack[nodeStack.length-1];
+    prev = chr;
+    chr =  input[idx++];
+    if (!validate(prev, chr)) throw('Error in parsing! ' + prev + ' ::: ' + chr);
+
+    if (chr == '{') {
+      let node = makeNode(++c, parent);
+      parent.children.push(node);
+      nodeStack.push(node);
+    } else if (chr == '}') {
+      nodeStack.pop();
+      c--;
+    } else if (chr == ',') {
+    } else if (chr == '_') {
+    }
+  } while (idx < input.length);
+  return tree;
+}
+
+const clean = (input) => {
+  let idx = 0, garbage = false, output = '';
+  while (idx < input.length) {
+    let chr = input[idx];
+    if (garbage) {
+      if (chr == '>') {
+        garbage = false;
+        idx++;
+      } else if (chr == '!') {
+        idx += 2;
+      } else {
+        idx++;
+      }
+    } else {
+      if (chr == '<') {
+        garbage = true;
+        output += '_'
+      } else {
+        output += chr;
+      }
+      idx++;
+    }
+  }
+  return output;
+}
+
+solve(INPUT);
