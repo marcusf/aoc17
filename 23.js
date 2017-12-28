2e16950c7dc8fa94abc6203c06e8da51d0bd057c
marcusf <marcusf@spotify.com>
2017-12-28 01:21:26 +0100

    Solved 23a

---
+++ 23.js
@@ -0,0 +1,51 @@
+const fs = require('fs');
+
+val = (y,reg) => isNaN(parseInt(y)) ? reg.get(y) : parseInt(y);
+
+const IL = {};
+
+IL['set'] = (x,y) => (reg) => { reg.set(x, val(y,reg)); reg.ip++; return reg; }
+IL['sub'] = (x,y) => (reg) => { reg.set(x, reg.get(x) - val(y,reg)); reg.ip++; return reg; }
+IL['mul'] = (x,y) => (reg) => { reg.set(x, reg.get(x) * val(y,reg)); reg.mulc++; reg.ip++; return reg; }
+IL['jnz'] = (x,y) => (reg) => { reg.ip += (val(x,reg) != 0 ? val(y,reg) : 1); return reg; }
+
+regs = (x,y) => { 
+  let r = []; 
+  if (x != undefined && isNaN(parseInt(x))) r.push(x); 
+  if (y != undefined && isNaN(parseInt(y))) r.push(y); 
+  return r;
+}
+
+let parse = (str) => {
+  let r = /([a-z]+) ([a-z]|(?:-?\d+))(?:\s([a-z]|(?:-?\d+)))?/.exec(str);
+  return {fun: IL[r[1]](r[2],r[3]), regs: regs(r[2],r[3])};
+}
+
+let setupReg = (reg, a) => {
+  let r = new Map(reg);
+  r.ip       = 0;
+  r.sndc     = 0;
+  r.block    = false;
+  r.mulc     = 0;
+  r.set('a', a);
+  return r;
+}
+
+let solve = (input) => {
+  let reg = new Map;
+
+  let instructions = input.split("\n").map((l) => {
+    let {fun, regs} = parse(l);
+    regs.forEach((r) => reg.set(r,0));
+    return fun;
+  });
+
+  reg = setupReg(reg, 0);
+  while (reg.ip < instructions.length) {
+    instructions[reg.ip](reg);
+  }
+  console.log(reg.mulc);
+}
+
+const INPUT = fs.readFileSync("23.txt", "utf8");
+solve(INPUT);
